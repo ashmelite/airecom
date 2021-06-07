3ed53fc66d0ee2f090273137c0d2edb13edcb9fe
ashmelite <ashutosh.mandan@gmail.com>
2021-06-07 21:50:14 +0530

    apply bootstrap theme on header, footer

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,9 +1,16 @@
 import React from 'react'
+import { Container, Row, Col } from 'react-bootstrap'
 
 const Footer = () => {
   return (
     <footer>
-      Footer
+      <Container>
+        <Row>
+          <Col className='text-center py-3'>
+            Copyright &copy; AirEcom
+          </Col>
+        </Row>
+      </Container>
     </footer>
   )
 }
--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,9 +1,21 @@
 import React from 'react'
+import { Navbar, Nav, Container } from 'react-bootstrap'
 
 const Header = () => {            //Arrow function
   return (
     <header>
-      Header
+      <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
+        <Container>
+          <Navbar.Brand href="/">AirEcom</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ml-auto">
+              <Nav.Link href="/cart">Cart</Nav.Link>
+              <Nav.Link href="/login">Sign In</Nav.Link>  
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
     </header>
   )
 }
