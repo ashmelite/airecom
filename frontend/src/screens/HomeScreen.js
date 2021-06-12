0219d03bd68a41d6971a943bfa55ebd0af8ceb80
ashmelite <ashutosh.mandan@gmail.com>
2021-06-12 22:43:00 +0530

    create loading & message components from bootstrap

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -2,6 +2,8 @@ import React, { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { Row, Col } from 'react-bootstrap'
 import Product from '../components/Product'
+import Loader from '../components/Loader'
+import Message from '../components/Message'
 import { listProducts } from '../actions/productActions'
 
 
@@ -20,7 +22,7 @@ const HomeScreen = () => {
   return (
     <>
       <h1>Latest Products</h1>
-      {loading ? <h2>Loading...</h2> : error ? <h3>{error}</h3> :             //error obj comes from backend
+      {loading ? <Loader /> : error ? <Message variant='danger'>{error}</Message> :             //error obj comes from backend
       
       <Row>
         {products.map((product) => (
