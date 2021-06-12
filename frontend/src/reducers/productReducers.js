8397251ce99b52843ee353bf8b6ff00c0090baf5
ashmelite <ashutosh.mandan@gmail.com>
2021-06-12 23:35:22 +0530

    general code cleanup

--- frontend/src/reducers/productReducers.js
+++ frontend/src/reducers/productReducers.js
@@ -22,15 +22,15 @@ export const productListReducer = (state = { products: [] }, action) => {       
 }
 
 
-export const productDetailsReducer = (state = { product: { reviews: [] } }, action) => {       //reducer takes in initial state (we can set it to an empty obj) and an action
+export const productDetailsReducer = (state = { product: { reviews: [] } }, action) => {
   switch (action.type) {
     case PRODUCT_DETAILS_REQUEST:
-      return { loading: true, ...state }                      //when we make a request, we want component to know that it's currently being fetched/loading 
-    case PRODUCT_DETAILS_SUCCESS:                //case for when we get a successful response back
-      return { loading: false, product: action.payload }         //see article 5.3 @ 04:00 for action.payload
+      return { loading: true, ...state }
+    case PRODUCT_DETAILS_SUCCESS:
+      return { loading: false, product: action.payload }
     case PRODUCT_DETAILS_FAIL:
       return { loading: false, error: action.payload }
     default:
-      return state                                      //return initial state (as passed in the argument)
+      return state
   }
 }
