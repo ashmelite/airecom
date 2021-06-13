220d664dd6d63b190600cf3b7144bb28215907f1
ashmelite <ashutosh.mandan@gmail.com>
2021-06-13 22:28:14 +0530

    add qty select option on product page, init cart

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import Header from './components/Header'
 import Footer from './components/Footer'
 import HomeScreen from './screens/HomeScreen'
 import ProductScreen from './screens/ProductScreen'
+import CartScreen from './screens/CartScreen'
 
 function App() {
   return (
@@ -13,7 +14,8 @@ function App() {
       <main className='py-3'>
         <Container>
           <Route path='/' component={HomeScreen} exact />
-          <Route path='/product/:id' component={ProductScreen} /> 
+          <Route path='/product/:id' component={ProductScreen} />
+          <Route path='/cart/:id?' component={CartScreen} />           {/* ? in :id makes id optional */}
         </Container>
       </main>
       <Footer />
