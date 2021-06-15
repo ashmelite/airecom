7317e21014b06e0b7df8c344c6cce0534768df92
ashmelite <ashutosh.mandan@gmail.com>
2021-06-15 22:04:02 +0530

    design cart page

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -23,10 +23,85 @@ const CartScreen = ({ match, location, history }) => {
     }
   }, [dispatch, productId, qty])
   
+  const removeFromCartHandler = (id) => {
+    console.log('remove')
+  }
+  
+  const checkoutHandler = () => {
+    history.push('/login?redirect=shipping')          //if user is not logged in, redirect to login page, else go to shipping page
+  }
+  
   return (
-    <div>
-      Cart
-    </div>
+    <Row>
+      <Col md={8}>
+        <h1>Shopping Cart</h1>
+        {
+          cartItems.length === 0 ? 
+          <Message>
+            Your cart is empty! <Link to='/'>Go Back</Link>
+          </Message> :
+          
+          (
+            <ListGroup variant='flush'>
+              {cartItems.map(item => (
+                <ListGroup.Item key={item.product}>           {/* item.product is the id */}
+                  <Row>
+                    <Col md={2}>
+                      <Image src={item.image} alt={item.name} fluid rounded />
+                    </Col>
+                    
+                    <Col md={3}>
+                      <Link to={`/product/${item.product}`}>{item.name}</Link>
+                    </Col>
+                    
+                    <Col md={2}>
+                      ${item.price}
+                    </Col>
+                    
+                    <Col md={2}>
+                      <Form.Control as='select' value={item.qty} onChange={(e) => dispatch(addToCart(item.product, Number(e.target.value)))}>
+                        {
+                          [...Array(item.countInStock).keys()].map(x => (
+                            <option key={x + 1} value={x + 1}>
+                              {x + 1}
+                            </option>
+                          ))
+                        }
+                      </Form.Control>
+                    </Col>
+                    
+                    <Col md={2}>
+                      <Button type='button' variant='light' onClick={() => removeFromCartHandler(item.product)}>
+                        <i className='fas fa-trash' />
+                      </Button>
+                    </Col>
+                  </Row>
+                </ListGroup.Item>
+              ))}
+            </ListGroup>
+          )
+          
+        }
+      </Col>
+      
+      <Col md={4}>
+        <Card>
+          <ListGroup variant='flush'>
+            <ListGroup.Item>
+              <h2>Subtotal ({cartItems.reduce((acc, itm) => acc + itm.qty, 0)}) items</h2>                {/*see article 6.4 @ 12:00 */}
+              
+              ${cartItems.reduce((acc, itm) => acc + itm.qty * itm.price, 0).toFixed(2)}
+            </ListGroup.Item>
+            
+            <ListGroup.Item>
+              <Button type='button' className='btn-block' disabled={cartItems.length === 0} onClick={checkoutHandler}>
+                Proceed to Checkout
+              </Button>
+            </ListGroup.Item>
+          </ListGroup>
+        </Card>
+      </Col>
+    </Row>
   )
 }
 
