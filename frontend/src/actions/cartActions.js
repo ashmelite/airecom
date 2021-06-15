70455eb5d66da17bbfac194adf37d43b0f64c369
ashmelite <ashutosh.mandan@gmail.com>
2021-06-15 22:18:31 +0530

    implement remove item from cart functionality

--- frontend/src/actions/cartActions.js
+++ frontend/src/actions/cartActions.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-import { CART_ADD_ITEM } from '../constants/cartConstants'
+import { CART_ADD_ITEM, CART_REMOVE_ITEM } from '../constants/cartConstants'
 
 export const addToCart = (id, qty) => async (dispatch, getState) => {
   const { data } = await axios.get(`/api/products/${id}`)
@@ -18,4 +18,14 @@ export const addToCart = (id, qty) => async (dispatch, getState) => {
   
   //save entire cart in local storage
   localStorage.setItem('cartItems', JSON.stringify(getState().cart.cartItems))          //JSON stringify to convert JavaScript obj to string. When reading back from storage use JSON.parse
+}
+
+
+export const removeFromCart = (id) => (dispatch, getState) => {
+  dispatch({
+    type: CART_REMOVE_ITEM,
+    payload: id
+  })
+  
+  localStorage.setItem('cartItems', JSON.stringify(getState().cart.cartItems))
 }
--- frontend/src/reducers/cartReducers.js
+++ frontend/src/reducers/cartReducers.js
@@ -1,4 +1,4 @@
-import { CART_ADD_ITEM } from '../constants/cartConstants'
+import { CART_ADD_ITEM, CART_REMOVE_ITEM } from '../constants/cartConstants'
 
 export const cartReducer = (state = { cartItems: [] }, action) => {
   switch(action.type) {
@@ -19,6 +19,13 @@ export const cartReducer = (state = { cartItems: [] }, action) => {
           cartItems: [...state.cartItems, item]
         }
       }
+      
+    case CART_REMOVE_ITEM:
+      return {
+        ...state,
+        cartItems: state.cartItems.filter(x => x.product !== action.payload)          //return every item whose id does not match with payload id, and filter out the item whose id matches
+      }
+      
     default:
       return state
   }
