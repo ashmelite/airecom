2ef94fc5c0aaa286eec4b8963b3362cbfce865d2
ashmelite <ashutosh.mandan@gmail.com>
2021-06-16 22:20:30 +0530

    create user auth route

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose'
+import bcrypt from 'bcryptjs'
 
 const userSchema = mongoose.Schema({
   
@@ -24,6 +25,11 @@ const userSchema = mongoose.Schema({
 }, {
   timestamps: true
 })
+
+//method to compare plain text password (entered by user) to the hashed password (which we encrypted with bcrypt while seeding data to db)
+userSchema.methods.matchPassword = async function(enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password)     //we can access current user's password (in db) by this keyword
+}
 
 const User = mongoose.model('User', userSchema)
 
--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from 'express'
 import dotenv from 'dotenv'
 import connectDB from './config/db.js'
 import productRoutes from './routes/productRoutes.js'
+import userRoutes from './routes/userRoutes.js'
 import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 
 dotenv.config()
@@ -10,11 +11,14 @@ connectDB()
 
 const app = express()
 
+app.use(express.json())       //allows us to accept json data in body and we can access the data from req.body
+
 app.get('/', (req, res) => {
   res.send('API is running...')
 })
 
 app.use('/api/products', productRoutes)
+app.use('/api/users', userRoutes)
 
 //handling 404 page errors
 app.use(notFound)
