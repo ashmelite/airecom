5bf5e1d191553aa0615879c5a4d15c90c70be317
ashmelite <ashutosh.mandan@gmail.com>
2021-06-16 23:08:14 +0530

    create auth middleware

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -28,6 +28,29 @@ const authUser = asyncHandler(async (req, res) => {
   
 })
 
+
+// @desc    Get user profile
+// @route   GET /api/users/profile
+// @access  Private
+const getUserProfile = asyncHandler(async (req, res) => {
+  
+  const user = await User.findById(req.user._id)      //user will exist in req since authMiddleware will run before this and if token is valid we can access user from req
+  
+  if(user) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin
+    })
+  } else {
+    res.status(404)
+    throw new Error('User not found!')
+  }
+  
+})
+
 export {
-  authUser
+  authUser,
+  getUserProfile
 }
---
+++ backend/middleware/authMiddleware.js
@@ -0,0 +1,36 @@
+import jwt from 'jsonwebtoken'
+import asyncHandler from 'express-async-handler'
+import User from '../models/userModel.js'
+
+const protect = asyncHandler(async (req, res, next) => {
+  let token
+  
+  //console.log(req.headers.authorization)
+  if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
+    try {
+      token = req.headers.authorization.split(' ')[1]
+      
+      const decoded = jwt.verify(token, process.env.JWT_SECRET)
+      
+      //put all user data in request so that the logged in user can have access to private routes
+      req.user = await User.findById(decoded.id).select('-password')
+      
+      //console.log(req.user)
+      
+      next()
+      
+    } catch (error) {
+      console.error(error)
+      res.status(401)
+      throw new Error('Not authorized, token is invalid')
+    }
+  }
+  
+  if (!token) {
+    res.status(401)
+    throw new Error('Not authorized, no token')
+  }
+  
+})
+
+export { protect }
