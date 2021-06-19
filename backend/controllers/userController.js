f83d3f209bcef6b843d20359cf3522d996ecc4ba
ashmelite <ashutosh.mandan@gmail.com>
2021-06-19 20:30:59 +0530

    implement route for creating a new user

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -29,6 +29,41 @@ const authUser = asyncHandler(async (req, res) => {
 })
 
 
+// @desc    Register a new user
+// @route   POST /api/users
+// @access  Public
+const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password } = req.body
+  
+  const userExists = await User.findOne({ email: email })
+  
+  if (userExists) {
+    res.status(400)
+    throw new Error('User already exists!')
+  }
+  
+  const user = await User.create({
+    name,
+    email,
+    password                          //password will get encrypted by mongodb since we added middleware (method) in the userModel
+  })
+  
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      token: generateToken(user._id)
+    })
+  } else {
+    res.status(400)
+    throw new Error('Invalid user data')
+  }
+  
+})
+
+
 // @desc    Get user profile
 // @route   GET /api/users/profile
 // @access  Private
@@ -52,5 +87,6 @@ const getUserProfile = asyncHandler(async (req, res) => {
 
 export {
   authUser,
-  getUserProfile
+  getUserProfile,
+  registerUser
 }
--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -31,6 +31,17 @@ userSchema.methods.matchPassword = async function(enteredPassword) {
   return await bcrypt.compare(enteredPassword, this.password)     //we can access current user's password (in db) by this keyword
 }
 
+//method to encrypt password before saving it to the database
+userSchema.pre('save', async function (next) {
+  
+  if (!this.isModified('password')) {         //when a user updates profile like name for eg, but not password then skip the hashing step below. We can skip using next()
+    next()
+  }
+  
+  const salt = await bcrypt.genSalt(10)
+  this.password = await bcrypt.hash(this.password, salt)
+})
+
 const User = mongoose.model('User', userSchema)
 
 export default User
