fee06841816f6e67766aa2d9cf00e2f18b9cde0f
ashmelite <ashutosh.mandan@gmail.com>
2021-06-20 19:17:05 +0530

    create route & method to update user profile

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -85,8 +85,42 @@ const getUserProfile = asyncHandler(async (req, res) => {
   
 })
 
+
+// @desc    Update user profile
+// @route   PUT /api/users/profile
+// @access  Private
+const updateUserProfile = asyncHandler(async (req, res) => {
+  
+  const user = await User.findById(req.user._id)
+  
+  if(user) {
+    user.name = req.body.name || user.name      // if request body has a name, set it to that name, else keep it the same
+    user.email = req.body.email || user.email
+    
+    if (req.body.password) {                    //check if password was sent through request
+      user.password = req.body.password
+    }
+    
+    const updatedUser = await user.save()
+    
+    res.json({
+      _id: updatedUser._id,
+      name: updatedUser.name,
+      email: updatedUser.email,
+      isAdmin: updatedUser.isAdmin,
+      token: generateToken(updatedUser._id)
+    })
+    
+  } else {
+    res.status(404)
+    throw new Error('User not found!')
+  }
+  
+})
+
 export {
   authUser,
   getUserProfile,
-  registerUser
+  registerUser,
+  updateUserProfile
 }
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,13 +1,13 @@
 import express from 'express'
-import { authUser, getUserProfile, registerUser } from '../controllers/userController.js'
+import { authUser, getUserProfile, registerUser, updateUserProfile } from '../controllers/userController.js'
 import { protect } from '../middleware/authMiddleware.js'
 
 const router = express.Router()
 
 
 router.route('/').post(registerUser)
-router.post('/login', authUser)
-router.route('/profile').get(protect, getUserProfile)
-
+router.post('/login', authUser)                       //execute this if it's a put request to /profile
+router.route('/profile').get(protect, getUserProfile).put(protect, updateUserProfile)
+                      //execute this if it's a get req
 
 export default router
