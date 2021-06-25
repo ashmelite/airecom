d7663284437daa60ac5dadc8ea2fb3c91f868b0e
ashmelite <ashutosh.mandan@gmail.com>
2021-06-25 16:34:06 +0530

    create endpoint for 'get order by id'

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -39,6 +39,24 @@ const addOrderItems = asyncHandler(async (req, res) => {
   
 })
 
+
+// @desc    Get order by ID
+// @route   GET /api/orders/:id
+// @access  Private
+const getOrderById = asyncHandler(async (req, res) => {
+  
+  const order = await Order.findById(req.params.id).populate('user', 'name, email')     //in that order, there is a user field (user id) which we want name and email back from
+  
+  if (order) {
+    res.json(order)
+  } else {
+    res.status(404)
+    throw new Error('Order not found!')
+  }
+  
+})
+
 export {
-  addOrderItems
+  addOrderItems,
+  getOrderById
 }
--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -1,10 +1,11 @@
 import express from 'express'
-import { addOrderItems } from '../controllers/orderController.js'
+import { addOrderItems, getOrderById } from '../controllers/orderController.js'
 import { protect } from '../middleware/authMiddleware.js'
 
 const router = express.Router()
 
 
 router.route('/').post(protect, addOrderItems)      //post request to /api/orders ; see server.js
+router.route('/:id').get(protect, getOrderById)     //keep this order below all other routes else it will look at api/orders/ as route without id
 
 export default router
