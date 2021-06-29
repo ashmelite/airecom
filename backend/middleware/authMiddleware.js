17e8e8726fc1ae7e18d435a44bbfbe0d20dc6da1
ashmelite <ashutosh.mandan@gmail.com>
2021-06-29 15:28:15 +0530

    create admin middleware (backend) & an admin route to get all users details

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -33,4 +33,14 @@ const protect = asyncHandler(async (req, res, next) => {
   
 })
 
-export { protect }
+
+const admin = (req, res, next) => {
+  if(req.user && req.user.isAdmin) {                // checking for req.user to make sure the user's logged in
+    next()
+  } else {
+    res.status(401)
+    throw new Error('Not authorized as an Admin!')
+  }
+}
+
+export { protect, admin }
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,10 +1,11 @@
 import express from 'express'
-import { authUser, getUserProfile, registerUser, updateUserProfile } from '../controllers/userController.js'
-import { protect } from '../middleware/authMiddleware.js'
+import { authUser, getUserProfile, registerUser, updateUserProfile, getUsers } from '../controllers/userController.js'
+import { protect, admin } from '../middleware/authMiddleware.js'
 
 const router = express.Router()
 
 
+router.route('/').get(protect, admin, getUsers)      //Admin route -> get all users
 router.route('/').post(registerUser)
 router.post('/login', authUser)                       //execute this if it's a put request to /profile
 router.route('/profile').get(protect, getUserProfile).put(protect, updateUserProfile)
