f68118f8889c842d48a2957fb69eeee4b6a61ae2
ashmelite <ashutosh.mandan@gmail.com>
2021-06-29 19:09:59 +0530

    create admin route for get user by id & update

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -13,7 +13,7 @@ const authUser = asyncHandler(async (req, res) => {
   
   const user = await User.findOne({ email: email })
   
-  if(user && (await user.matchPassword(password))) {
+  if (user && (await user.matchPassword(password))) {
     res.json({
       _id: user._id,
       name: user.name,
@@ -71,7 +71,7 @@ const getUserProfile = asyncHandler(async (req, res) => {
   
   const user = await User.findById(req.user._id)      //user will exist in req since authMiddleware will run before this and if token is valid we can access user from req
   
-  if(user) {
+  if (user) {
     res.json({
       _id: user._id,
       name: user.name,
@@ -93,7 +93,7 @@ const updateUserProfile = asyncHandler(async (req, res) => {
   
   const user = await User.findById(req.user._id)
   
-  if(user) {
+  if (user) {
     user.name = req.body.name || user.name      // if request body has a name, set it to that name, else keep it the same
     user.email = req.body.email || user.email
     
@@ -147,11 +147,59 @@ const deleteUser = asyncHandler(async (req, res) => {
   
 })
 
+
+// @desc    Get user by ID
+// @route   GET /api/users/:id
+// @access  Private/Admin
+const getUserById = asyncHandler(async (req, res) => {
+  
+  const user = await User.findById(req.params.id).select('-password')
+  
+  if (user) {
+    res.json(user)
+  } else {
+    res.status(404)
+    throw new Error('User not found!')
+  }
+  
+})
+
+
+// @desc    Update user
+// @route   PUT /api/users/:id
+// @access  Private/Admin
+const updateUser = asyncHandler(async (req, res) => {
+  
+  const user = await User.findById(req.params.id)
+  
+  if (user) {
+    user.name = req.body.name || user.name
+    user.email = req.body.email || user.email
+    user.isAdmin = req.body.isAdmin               //see article 11.5 @ 4:00 to know why we didn't check for OR condition here
+    
+    const updatedUser = await user.save()
+    
+    res.json({
+      _id: updatedUser._id,
+      name: updatedUser.name,
+      email: updatedUser.email,
+      isAdmin: updatedUser.isAdmin
+    })
+    
+  } else {
+    res.status(404)
+    throw new Error('User not found!')
+  }
+  
+})
+
 export {
   authUser,
   getUserProfile,
   registerUser,
   updateUserProfile,
   getUsers,
-  deleteUser
+  deleteUser,
+  getUserById,
+  updateUser
 }
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,16 +1,16 @@
 import express from 'express'
-import { authUser, getUserProfile, registerUser, updateUserProfile, getUsers, deleteUser } from '../controllers/userController.js'
+import { authUser, getUserProfile, registerUser, updateUserProfile, getUsers, deleteUser, getUserById, updateUser } from '../controllers/userController.js'
 import { protect, admin } from '../middleware/authMiddleware.js'
 
 const router = express.Router()
 
 
-router.route('/').get(protect, admin, getUsers)      //Admin route -> get all users
-router.route('/').post(registerUser)
+router.route('/').post(registerUser).get(protect, admin, getUsers)            //get request is an Admin route
 router.post('/login', authUser)                       //execute this if it's a put request to /profile
 router.route('/profile').get(protect, getUserProfile).put(protect, updateUserProfile)
-                      //execute this if it's a get req
+                      //execute this if it's a get req; means we can chain multiple types of request if route is same
                                           
-router.route('/:id').delete(protect, admin, deleteUser)
+router.route('/:id').delete(protect, admin, deleteUser).get(protect, admin, getUserById).put(protect, admin, updateUser)
+
                       
 export default router
