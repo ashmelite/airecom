0da67e3e8404ed2adb68996c92c83647d6ffa6e0
ashmelite <ashutosh.mandan@gmail.com>
2021-06-29 16:37:07 +0530

    clear userlist state on logout & redirect to login (bug fix)

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -8,6 +8,7 @@ import {
   USER_DETAILS_SUCCESS,
   USER_LIST_FAIL,
   USER_LIST_REQUEST,
+  USER_LIST_RESET,
   USER_LIST_SUCCESS,
   USER_LOGIN_FAIL, 
   USER_LOGIN_REQUEST, 
@@ -63,6 +64,7 @@ export const logout = () => (dispatch) => {
   dispatch({ type: ORDER_CREATE_RESET })
   dispatch({ type: ORDER_LIST_MY_RESET })
   dispatch({ type: ORDER_DETAILS_RESET })
+  dispatch({ type: USER_LIST_RESET })
 }
 
 
