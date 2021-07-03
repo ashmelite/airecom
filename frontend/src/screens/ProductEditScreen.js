2944721fa44908c601220534dab95653b8203647
ashmelite <ashutosh.mandan@gmail.com>
2021-07-03 23:10:16 +0530

    implement update product functionality

--- frontend/src/screens/ProductEditScreen.js
+++ frontend/src/screens/ProductEditScreen.js
@@ -5,7 +5,8 @@ import { useDispatch, useSelector } from 'react-redux'
 import Message from '../components/Message'
 import Loader from '../components/Loader'
 import FormContainer from '../components/FormContainer'
-import { listProductDetails } from '../actions/productActions'
+import { listProductDetails, updateProduct } from '../actions/productActions'
+import { PRODUCT_UPDATE_RESET } from '../constants/productConstants'
 
 const ProductEditScreen = ({ match, history }) => {
   
@@ -24,9 +25,15 @@ const ProductEditScreen = ({ match, history }) => {
   const productDetails = useSelector(state => state.productDetails)
   const { loading, error, product } = productDetails
   
+  const productUpdate = useSelector(state => state.productUpdate)
+  const { loading: loadingUpdate, error: errorUpdate, success: successUpdate } = productUpdate
   
   useEffect(() => {
     
+    if (successUpdate) {
+      dispatch({ type: PRODUCT_UPDATE_RESET })
+      history.push('/admin/productlist')
+    } else {
       if (!product.name || product._id !== productId) {
         dispatch(listProductDetails(productId))
       } else {
@@ -38,14 +45,24 @@ const ProductEditScreen = ({ match, history }) => {
         setCountInStock(product.countInStock)
         setDescription(product.description)
       }
-      
-  }, [product, dispatch, productId, history])
+    }
+    
+  }, [product, dispatch, productId, history, successUpdate])
   
   
   const submitHandler = (e) => {
     e.preventDefault()
     
-    //Update Product
+    dispatch(updateProduct({ 
+      _id: productId,
+      name: name,
+      price: price,
+      image: image,
+      brand: brand,
+      category: category,
+      description: description,
+      countInStock: countInStock
+     }))
     
   }
   
@@ -57,7 +74,8 @@ const ProductEditScreen = ({ match, history }) => {
       
       <FormContainer>
         <h1>Edit Product</h1>
-        
+        { loadingUpdate && <Loader /> }
+        { errorUpdate && <Message variant='danger'>{errorUpdate}</Message> }
         {
           loading ? <Loader /> : error ? <Message variant='danger'>{error}</Message> :
           (
