de0e3d75e34f629aedd078f9f2aa6eea8a353cfc
ashmelite <ashutosh.mandan@gmail.com>
2021-07-19 21:38:20 +0530

    pre-requisites for deployment

--- backend/server.js
+++ backend/server.js
@@ -23,14 +23,11 @@ if(process.env.NODE_ENV === 'development') {
 
 app.use(express.json())       //allows us to accept json data in body and we can access the data from req.body
 
-app.get('/', (req, res) => {
-  res.send('API is running...')
-})
-
 app.use('/api/products', productRoutes)
 app.use('/api/users', userRoutes)
 app.use('/api/orders', orderRoutes)
 app.use('/api/upload', uploadRoutes)
+
 
 //fetch paypal client id in frontend using this route
 app.get('/api/config/paypal', (req, res) => res.send(process.env.PAYPAL_CLIENT_ID))
@@ -46,6 +43,19 @@ you must configure it to serve the static content you want it to serve.
 //make uploads folder static; see article 12.7 @ 12:00
 const __dirname = path.resolve()
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
+
+
+//when we deploy our app; for more info see article 14.1
+if(process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/frontend/build')))               //make frontend/build a static folder
+
+  app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html')))
+
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running...')
+  })
+}
 
 
 //handling 404 page errors
