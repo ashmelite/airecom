53a227d97eb8db182e1d475c23a5d17c20f8a096
ashmelite <ashutosh.mandan@gmail.com>
2021-07-24 00:47:34 +0530

    theme update

--- frontend/src/components/ProductCarousel.js
+++ frontend/src/components/ProductCarousel.js
@@ -21,7 +21,7 @@ const ProductCarousel = () => {
     (
       <Carousel pause='hover' className='bg-dark'>
         {products.map(product => (
-          <Carousel.Item key={product._id}>
+          <Carousel.Item key={product._id} interval='3000'>
             <Link to={`/product/${product._id}`}>
               <Image src={product.image} alt={product.name} fluid />
               <Carousel.Caption className='carousel-caption'>
